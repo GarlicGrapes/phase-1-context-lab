c6a23984f0336f0f180ffc143cb8f7311a9440f9
Austin Longo <austinlongocontracting@gmail.com>
2022-01-19 21:23:55 -0500

    commit tree c596e72c6527cb797ba9507b84b20b9d8ea2f0d8

--- index.js
+++ index.js
@@ -21,3 +21,90 @@ const allWagesFor = function () {
     return payable
 }
 
+function createEmployeeRecord (record) {
+    return {
+        firstName: record[0],
+        familyName: record[1],
+        title: record[2],
+        payPerHour: record[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(records) {
+    let temp = []
+    records.forEach(element => temp.push(createEmployeeRecord(element)))
+    return temp
+}
+
+
+function createTimeInEvent (timeEvent) {
+   let buildInTimeEvent = {
+        type: 'TimeIn',
+        date: timeEvent.split(' ')[0],
+        hour: parseInt(timeEvent.split(' ')[1])  
+    }
+    this.timeInEvents.push(buildInTimeEvent)
+    return  this
+}
+
+function createTimeOutEvent (timeEvent) {
+    let buildOutTimeEvent = {
+         type: 'TimeOut',
+         date: timeEvent.split(' ')[0],
+         hour: parseInt(timeEvent.split(' ')[1])
+     }
+     this.timeOutEvents.push(buildOutTimeEvent)
+     return  this
+ }
+
+ function hoursWorkedOnDate(date){
+    //return ((employee.timeOutEvents[0].hour - employee.timeInEvents[0].hour) / 100)
+    let timeIn = 0
+    let timeOut = 0
+    
+    for (let each of this.timeInEvents) {
+        if (each.date == date) {timeIn = each.hour}
+    }
+    for (let each of this.timeOutEvents) {
+        if (each.date == date) {timeOut = each.hour}
+    }
+    return (timeOut - timeIn) / 100
+
+
+}
+
+ function wagesEarnedOnDate(date){
+    return (this.payPerHour * hoursWorkedOnDate.call(this, date))
+ }
+
+//  function allWagesFor(employee){
+//     // let inTimes = []
+//     // let outTimes = []
+//     //let timeHolder = []
+//     let totalHours = 0
+
+//     for (var each of employee.timeInEvents) {
+//         totalHours += hoursWorkedOnDate(employee, each.date)
+//     }
+      
+//     return ((totalHours) * employee.payPerHour)
+//  }
+
+ function findEmployeeByFirstName (srcArray, firstName) {
+     for (let each of srcArray) {
+         if (each.firstName == firstName) {return each}
+     }
+ }
+
+//  function calculatePayroll (employeeArray) {
+//     return employeeArray.reduce(function(each, calc){
+//         return each + allWagesFor.call(calc)
+//     }, 0)
+//  }
+let calculatePayroll = function(arrayOfEmployeeRecords){
+    return arrayOfEmployeeRecords.reduce(function(memo, rec){
+        return memo + allWagesFor.call(rec)
+    }, 0)
+}
